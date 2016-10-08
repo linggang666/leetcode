512eba5385efcd6986fb880aad377ad5b3896c16
liulinggang <liulinggang@xiongying.com>
2016-10-08 16:26:56 +0800

    fix

--- src/p134/V1.java
+++ src/p134/V1.java
@@ -4,23 +4,30 @@ public class V1 {
 
 	public int canCompleteCircuit(int[] gas, int[] cost) {
 		
-		int start = 0, sum=0, i=0;
-		while(i < gas.length){
+		int len = gas.length;
+		int diff = 0;
+		for (int i = 0; i < len; i++) {
+			diff += gas[i] - cost[i];
+		}
+		if(diff < 0 )return -1;
+		
+		int index = 0, sum=0, i=0;
+		while(i < len){
 			sum += gas[i] - cost[i];
 			if(sum < 0){
 				sum = 0;
-				start = i+1;
+				index = i+1;
 			}
 			i++;
 		}
 		
-		return sum>=0&&start<gas.length ?start :-1;
+		return index;
 	}
 
 	public static void main(String[] args) {
 		V1 v = new V1();
-		int[] arr1 = {2,4};
-		int[] arr2 = {3,4};
+		int[] arr1 = {};
+		int[] arr2 = {};
 		
 		System.out.println(v.canCompleteCircuit(arr1, arr2));
 	}
