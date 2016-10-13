4acc8b23231627a0490d4931512399ba99fcb960
liulinggang <liulinggang@xiongying.com>
2016-10-13 17:55:56 +0800

    Self Crossing

---
+++ src/p335/V1.java
@@ -0,0 +1,19 @@
+package p335;
+
+public class V1 {
+	public boolean isSelfCrossing(int[] x) {
+		for (int i = 3; i < x.length; i++) {
+			if(x[i]>=x[i-2] && x[i-3]>=x[i-1]){
+				return true;
+			}
+			if(i>=4 && x[i-1]==x[i-3] || x[i]+x[i-4]>=x[i-2]){
+				return true;
+			}
+			if(i>=5 && x[i-2]>=x[i-4] && x[i-3]>=x[i-1] && x[i-1]+x[i-5]>x[i-3] && x[i]+x[i-4]>=x[i-2]){
+				return true;
+			}
+		}
+		
+		return false;
+	}
+}
