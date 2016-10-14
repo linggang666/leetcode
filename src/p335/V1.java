a0ab98812c6f35730515a79c117851d65568dff1
liulinggang <liulinggang@xiongying.com>
2016-10-14 15:59:08 +0800

    fix

--- src/p335/V1.java
+++ src/p335/V1.java
@@ -6,14 +6,13 @@ public class V1 {
 			if(x[i]>=x[i-2] && x[i-3]>=x[i-1]){
 				return true;
 			}
-			if(i>=4 && x[i-1]==x[i-3] || x[i]+x[i-4]>=x[i-2]){
+			if(i>=4 && x[i-1]==x[i-3] && x[i]+x[i-4]>=x[i-2]){
 				return true;
 			}
-			if(i>=5 && x[i-2]>=x[i-4] && x[i-3]>=x[i-1] && x[i-1]+x[i-5]>x[i-3] && x[i]+x[i-4]>=x[i-2]){
+			if(i>=5 && x[i-2]>=x[i-4] && x[i-3]>=x[i-1] && x[i-1]+x[i-5]>=x[i-3] && x[i]+x[i-4]>=x[i-2]){
 				return true;
 			}
 		}
-		
 		return false;
 	}
 }
