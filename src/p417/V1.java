9806afdf8c8d4d238fd9fea77f584d0e25494c0d
liulinggang <liulinggang@xiongying.com>
2016-10-31 17:34:01 +0800

    Pacific Atlantic Water Flow

---
+++ src/p417/V1.java
@@ -0,0 +1,58 @@
+package p417;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class V1 {
+	
+	public List<int[]> pacificAtlantic(int[][] matrix) {
+		List<int[]> res = new ArrayList<int[]>();
+		if(matrix.length<1 || matrix[0].length<1) 
+			return res;
+		
+		int rows = matrix.length, cols = matrix[0].length;
+		boolean[][] pacificVisited = new boolean[rows][cols];
+		boolean[][] atlantiVisited = new boolean[rows][cols];
+		
+		for (int i = 0; i < rows; i++) {
+			canFlow(matrix, pacificVisited, 0, i, 0);
+			canFlow(matrix, atlantiVisited, 0, i, cols-1);
+		}
+		for (int i = 0; i < cols; i++) {
+			canFlow(matrix, pacificVisited, 0, 0, i);
+			canFlow(matrix, atlantiVisited, 0, rows-1, i);
+		}
+		
+		for (int i = 0; i < rows; i++) {
+			for (int j = 0; j < cols; j++) {
+				if(pacificVisited[i][j] && atlantiVisited[i][j]){
+					res.add(new int[]{i,j});
+				}
+			}
+		}
+		return res;
+	}
+	
+	
+	private void canFlow(int[][] matrix, boolean[][] visited, int h, int i, int j) {
+		if(i<0 || i>=visited.length || j<0 || j>=matrix[0].length || visited[i][j] || matrix[i][j]<h){
+			return ;
+		}
+		visited[i][j] = true;
+		canFlow(matrix, visited, matrix[i][j], i-1, j);
+		canFlow(matrix, visited, matrix[i][j], i+1, j);
+		canFlow(matrix, visited, matrix[i][j], i, j-1);
+		canFlow(matrix, visited, matrix[i][j], i, j+1);
+	}
+	
+	
+	public static void main(String[] args) {
+		int[][] c = {
+				{1,2,3},	
+				{8,9,4},	
+				{7,6,5},	
+		};
+		V1 v = new V1();
+		v.pacificAtlantic(c);
+	}
+}
